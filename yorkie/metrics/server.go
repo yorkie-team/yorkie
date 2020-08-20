c7760797f959318027a415da039fe5c34e5d25fc
hackerwins <susukang98@gmail.com>
2020-08-21 08:32:51 +0900

    Rename metrics/metrics_server.go to metrics/server.go

--- yorkie/metrics/server.go
+++ yorkie/metrics/server.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-
 	"github.com/yorkie-team/yorkie/pkg/log"
 	"github.com/yorkie-team/yorkie/pkg/version"
 )
