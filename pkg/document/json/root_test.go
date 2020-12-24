671076c20244c6626803dab2f04a562577ab498c
dc7303 <dc7303@gmail.com>
2020-12-25 00:14:38 +0900

    Cleanup garbage collection test for text type
    Organized so that garbage collection tests for text types can be managed
    in a location that fits the scope of responsibility.

--- pkg/document/json/root_test.go
+++ pkg/document/json/root_test.go
@@ -64,24 +64,30 @@ func TestRoot(t *testing.T) {
 		assert.Equal(t, `"Hello World"`, text.Marshal())
 		assert.Equal(t, 0, root.GarbageLen())
 
-		fromPos, toPos = text.CreateRange(6, 11)
+		fromPos, toPos = text.CreateRange(5, 10)
 		text.Edit(fromPos, toPos, nil, "Yorkie", ctx.IssueTimeTicket())
 		registerRemovedNodeTextElement(fromPos, toPos, root, text)
-		assert.Equal(t, `"Hello Yorkie"`, text.Marshal())
+		assert.Equal(t, `"HelloYorkied"`, text.Marshal())
 		assert.Equal(t, 1, root.GarbageLen())
 
-		fromPos, toPos = text.CreateRange(0, 6)
+		fromPos, toPos = text.CreateRange(0, 5)
+		text.Edit(fromPos, toPos, nil, "", ctx.IssueTimeTicket())
+		registerRemovedNodeTextElement(fromPos, toPos, root, text)
+		assert.Equal(t, `"Yorkied"`, text.Marshal())
+		assert.Equal(t, 2, root.GarbageLen())
+
+		fromPos, toPos = text.CreateRange(6, 7)
 		text.Edit(fromPos, toPos, nil, "", ctx.IssueTimeTicket())
 		registerRemovedNodeTextElement(fromPos, toPos, root, text)
 		assert.Equal(t, `"Yorkie"`, text.Marshal())
-		assert.Equal(t, 2, root.GarbageLen())
+		assert.Equal(t, 3, root.GarbageLen())
 
 		// It contains code marked tombstone.
 		// After calling the garbage collector, the node will be removed.
 		nodeLen := len(text.Nodes())
-		assert.Equal(t, 3, nodeLen)
+		assert.Equal(t, 4, nodeLen)
 
-		assert.Equal(t, 2, root.GarbageCollect(time.MaxTicket))
+		assert.Equal(t, 3, root.GarbageCollect(time.MaxTicket))
 		assert.Equal(t, 0, root.GarbageLen())
 		nodeLen = len(text.Nodes())
 		assert.Equal(t, 1, nodeLen)
