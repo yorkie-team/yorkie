85f8262685b84b10c0b612264bf6a2bd7c168ddf
computerphilosopher <erlk0nig@naver.com>
2021-04-03 17:37:05 +0900

    Add test cases for ticket

--- pkg/document/time/ticket_test.go
+++ pkg/document/time/ticket_test.go
@@ -21,4 +21,41 @@ func TestTicket(t *testing.T) {
 		ticket := time.NewTicket(0, 0, actorID)
 		assert.Equal(t, actorID.Bytes(), ticket.ActorIDBytes())
 	})
+
+	t.Run("constructor and getter method test", func(t *testing.T) {
+		actorID, _ := time.ActorIDFromHex("0123456789abcdef01234567")
+		ticket := time.NewTicket(0, 1, actorID)
+		assert.Equal(t, uint64(0), ticket.Lamport())
+		assert.Equal(t, uint32(1), ticket.Delimiter())
+		assert.Equal(t, actorID, ticket.ActorID())
+
+		assert.Equal(t, "0:1:"+ticket.ActorIDHex(), ticket.Key())
+		assert.Equal(t, "0:1:"+ticket.ActorIDHex()[22:24],
+			ticket.AnnotatedString())
+		ticket = ticket.SetActorID(nil)
+		assert.Equal(t, "0:1:", ticket.Key())
+		assert.Equal(t, "0:1:nil", ticket.AnnotatedString())
+	})
+
+	t.Run("ticket comparing test", func(t *testing.T) {
+		beforeActorID, _ := time.ActorIDFromHex("0000000000abcdef01234567")
+		afterActorID, _ := time.ActorIDFromHex("0123456789abcdef01234567")
+
+		before := time.NewTicket(0, 0, beforeActorID)
+		after := time.NewTicket(1, 0, afterActorID)
+		assert.True(t, after.After(before))
+		assert.False(t, before.After(after))
+
+		before = time.NewTicket(0, 0, beforeActorID)
+		after = time.NewTicket(0, 0, afterActorID)
+		assert.True(t, after.After(before))
+		assert.False(t, before.After(after))
+
+		before = time.NewTicket(0, 0, beforeActorID)
+		after = time.NewTicket(0, 1, beforeActorID)
+		assert.True(t, after.After(before))
+		assert.False(t, before.After(after))
+
+		assert.False(t, before.After(before))
+	})
 }
