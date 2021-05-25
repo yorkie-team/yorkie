6396ecb93dcf4ddb9ef055ba270a1ea48145b366
Hackerwins <susukang98@gmail.com>
2021-05-25 10:35:57 +0900

    Update client test with token

--- client/client_test.go
+++ client/client_test.go
@@ -3,16 +3,18 @@ package client_test
 import (
 	"testing"
 
+	"github.com/rs/xid"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/yorkie-team/yorkie/client"
 )
 
 func TestClient(t *testing.T) {
-	t.Run("new test", func(t *testing.T) {
-		opts := client.Option{Metadata: map[string]string{
-			"Name": "ClientName",
-		}}
+	t.Run("create instance test", func(t *testing.T) {
+		opts := client.Option{
+			Token: xid.New().String(),
+			Metadata: map[string]string{"Name": "ClientName"},
+		}
 		cli, err := client.NewClient(opts)
 		assert.NoError(t, err)
 
