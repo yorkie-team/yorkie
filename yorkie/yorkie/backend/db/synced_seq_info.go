a921a8e4a3f5a80ebac9cbc3a8cc9a66fdcdd830
jhp@fount.co <parkjh688@gmail.com>
2021-08-26 14:55:31 +0900

    Please enter the commit message for your changes. Lines starting

--- yorkie/yorkie/backend/db/synced_seq_info.go
+++
@@ -1,9 +0,0 @@
-package db
-
-// SyncedSeqInfo is a structure representing information about the synchronized
-// sequence for each client.
-type SyncedSeqInfo struct {
-	DocID     ID     `bson:"doc_id_fake"`
-	ClientID  ID     `bson:"client_id_fake"`
-	ServerSeq uint64 `bson:"server_seq"`
-}
