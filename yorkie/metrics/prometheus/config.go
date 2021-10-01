500f5fc3cdc42f2658545adb00fe1256f9b33a66
Jinsu Park <dev.umijs@gmail.com>
2021-10-01 09:20:10 +0900

    typo

--- yorkie/metrics/prometheus/config.go
+++ yorkie/metrics/prometheus/config.go
@@ -7,7 +7,7 @@ import (
 
 var (
 	// ErrInvalidMetricPort occurs when the port in the config is invalid.
-	ErrInvalidMetricPort = errors.New("invalid port number for Metric server")
+	ErrInvalidMetricPort = errors.New("invalid port number for metric server")
 )
 
 // Config is the configuration for creating a Server instance.
@@ -15,7 +15,7 @@ type Config struct {
 	Port int
 }
 
-// Validate validates the port number and the files for certification.
+// Validate validates the port number.
 func (c *Config) Validate() error {
 	if c.Port < 1 || 65535 < c.Port {
 		return fmt.Errorf("must be between 1 and 65535, given %d: %w", c.Port, ErrInvalidMetricPort)
