63dfcb606170672e5f93e7533ce7c1257f3ac185
DongjinS <sdjsdj3@gmail.com>
2022-06-28 18:57:15 +0900

    Implement gRPC benchmark test.
    gRPC benchmark is consists three test:
    client to server / client to client via server / admin to server.
    
    "client to client via server" test show error sometimes. The cause
     is not clear yet, but it seems to be due to concurrency.

--- test/bench/grpc_bench_test.go
+++ test/bench/grpc_bench_test.go
@@ -21,10 +21,12 @@ package bench
 import (
 	"context"
 	"fmt"
-	"os"
+	"io"
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/yorkie-team/yorkie/admin"
 	"github.com/yorkie-team/yorkie/api/types"
 	"github.com/yorkie-team/yorkie/client"
 	"github.com/yorkie-team/yorkie/pkg/document"
@@ -35,124 +37,182 @@ import (
 	"github.com/yorkie-team/yorkie/test/helper"
 )
 
-type clientAndDocPair struct {
-	cli *client.Client
-	doc *document.Document
-}
-
 var defaultServer *server.Yorkie
 
-func TestMain(m *testing.M) {
+func startDefaultServer() {
 	svr := helper.TestServer()
 	if err := svr.Start(); err != nil {
 		logging.DefaultLogger().Fatal(err)
 	}
 	defaultServer = svr
-	code := m.Run()
-	if defaultServer != nil {
-		if err := defaultServer.Shutdown(true); err != nil {
-			logging.DefaultLogger().Error(err)
-		}
-	}
-	os.Exit(code)
 }
 
-// activeClient is a helper function to create active clients.
-func activeClients(B *testing.B, n int) (clients []*client.Client) {
-	for i := 0; i < n; i++ {
-		c, err := client.Dial(
-			defaultServer.RPCAddr(),
-			client.WithPresence(types.Presence{"name": fmt.Sprintf("name-%d", i)}),
-		)
-		assert.NoError(B, err)
-
-		err = c.Activate(context.Background())
-		assert.NoError(B, err)
-
-		clients = append(clients, c)
+func benchmarkUpdateAndSync(cnt int, ctx context.Context, b *testing.B, cli *client.Client, d *document.Document, key string) error {
+	for i := 0; i < cnt; i++ {
+		err := d.Update(func(root *proxy.ObjectProxy) error {
+			text := root.GetText(key)
+			text.Edit(0, 0, "c")
+			return nil
+		})
+		assert.NoError(b, err)
+		err = cli.Sync(ctx)
+		assert.NoError(b, err)
 	}
-	return
+	return nil
 }
 
-// cleanupClients is a helper function to clean up clients.
-func cleanupClients(b *testing.B, clients []*client.Client) {
-	for _, c := range clients {
-		assert.NoError(b, c.Deactivate(context.Background()))
-		assert.NoError(b, c.Close())
-	}
-}
-
-func syncClientsThenAssertEqual(t *testing.B, pairs []clientAndDocPair) {
-	assert.True(t, len(pairs) > 1)
+func benchmarkUpdateProject(cnt int, adminCli *admin.Client, b *testing.B) error {
 	ctx := context.Background()
-	// Save own changes and get previous changes.
-	for i, pair := range pairs {
-		fmt.Printf("before d%d: %s\n", i+1, pair.doc.Marshal())
-		err := pair.cli.Sync(ctx)
-		assert.NoError(t, err)
-	}
+	for i := 0; i < cnt; i++ {
+		name := fmt.Sprintf("name%d", i)
+		authWebhookURL := fmt.Sprintf("authWebhookURL%d", i)
+		authWebhookMethods := []string{}
+		for _, m := range types.AuthMethods() {
+			authWebhookMethods = append(authWebhookMethods, string(m))
+		}
 
-	// Get last client changes.
-	// Last client get all precede changes in above loop.
-	for _, pair := range pairs[:len(pairs)-1] {
-		err := pair.cli.Sync(ctx)
-		assert.NoError(t, err)
+		_, err := adminCli.UpdateProject(
+			ctx,
+			"000000000000000000000000",
+			&types.UpdatableProjectFields{
+				Name:               &name,
+				AuthWebhookURL:     &authWebhookURL,
+				AuthWebhookMethods: &authWebhookMethods,
+			},
+		)
+		assert.NoError(b, err)
 	}
-
-	// Assert start.
-	expected := pairs[0].doc.Marshal()
-	fmt.Printf("after d1: %s\n", expected)
-	for i, pair := range pairs[1:] {
-		v := pair.doc.Marshal()
-		fmt.Printf("after d%d: %s\n", i+2, v)
-		assert.Equal(t, expected, v)
-	}
+	return nil
 }
 
+func benchmarkWatchAndUpdate(cnt int, wg *sync.WaitGroup, b *testing.B) {
+	defer wg.Done()
+	cli, err := client.Dial(
+		defaultServer.RPCAddr(),
+		client.WithPresence(types.Presence{"name": fmt.Sprintf("name-%s", b.Name())}),
+	)
+	assert.NoError(b, err)
+	defer func() {
+		err := cli.Close()
+		assert.NoError(b, err)
+	}()
+
+	ctx := context.Background()
+	err = cli.Activate(ctx)
+	assert.NoError(b, err)
+	assert.True(b, cli.IsActive())
+
+	d := document.New(key.Key(b.Name()))
+	err = cli.Attach(ctx, d)
+	assert.NoError(b, err)
+	docKey := "testKey"
+	err = d.Update(func(root *proxy.ObjectProxy) error {
+		root.SetNewText(docKey)
+		return nil
+	})
+	assert.NoError(b, err)
+
+	rch, err := cli.Watch(ctx, d)
+	assert.NoError(b, err)
+
+	innerWg := sync.WaitGroup{}
+	innerWg.Add(2)
+	done := make(chan bool)
+	go func() {
+		defer innerWg.Done()
+		for {
+			select {
+			case resp := <-rch:
+				if resp.Err == io.EOF {
+					assert.Fail(b, resp.Err.Error())
+				}
+				assert.NoError(b, resp.Err)
+
+				if resp.Type == client.DocumentsChanged {
+					err := cli.Sync(ctx, resp.Keys...)
+					assert.NoError(b, err)
+				}
+			case <-done:
+				return
+			}
+		}
+	}()
+	go func() {
+		defer innerWg.Done()
+		benchmarkUpdateAndSync(cnt, ctx, b, cli, d, docKey)
+		done <- true
+	}()
+	innerWg.Wait()
+}
 
 func BenchmarkRPC(b *testing.B) {
-	svr, err := server.New(helper.TestConfig())
-	assert.NoError(b, err)
-	assert.NoError(b, svr.Start())
-	defer func() { assert.NoError(b, svr.Shutdown(true)) }()
+	b.StopTimer()
+	startDefaultServer()
+	defer func() {
+		if defaultServer != nil {
+			if err := defaultServer.Shutdown(true); err != nil {
+				logging.DefaultLogger().Error(err)
+			}
+		}
+	}()
+	b.StartTimer()
+	b.Run("client to server", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			cli, err := client.Dial(
+				defaultServer.RPCAddr(),
+				client.WithPresence(types.Presence{"name": fmt.Sprintf("name-%s", b.Name())}),
+			)
+			assert.NoError(b, err)
+			defer func() {
+				err := cli.Close()
+				assert.NoError(b, err)
+			}()
 
-	clients := activeClients(b, 2)
-	c1, c2 := clients[0], clients[1]
-	defer cleanupClients(b, clients)
-	b.Run("rpc between 2 client test", func(b *testing.B) {
-		ctx := context.Background()
-		d1 := document.New(key.Key(b.Name()))
-		err := c1.Attach(ctx, d1)
-		assert.NoError(b, err)
+			ctx := context.Background()
+			err = cli.Activate(ctx)
+			assert.NoError(b, err)
+			assert.True(b, cli.IsActive())
 
-		err = d1.Update(func(root *proxy.ObjectProxy) error {
-			root.SetNewObject("k1")
-			return nil
-		}, "set v1 by c1")
-		assert.NoError(b, err)
-		assert.Equal(b, `{"k1":{}}`, d1.Marshal())
-		err = c1.Sync(ctx)
-		assert.NoError(b, err)
+			d1 := document.New("doc1")
+			err = cli.Attach(ctx, d1)
+			assert.NoError(b, err)
 
-		d2 := document.New(key.Key(b.Name()))
-		err = c2.Attach(ctx, d2)
-		assert.NoError(b, err)
+			testKey := "testKey"
+			err = d1.Update(func(root *proxy.ObjectProxy) error {
+				root.SetNewText(testKey)
+				return nil
+			})
+			assert.NoError(b, err)
 
-		err = d1.Update(func(root *proxy.ObjectProxy) error {
-			root.Delete("k1")
-			root.SetString("k1", "v1")
-			return nil
-		}, "delete and set v1 by c1")
-		assert.NoError(b, err)
-		assert.Equal(b, `{"k1":"v1"}`, d1.Marshal())
+			b.StartTimer()
+			err = benchmarkUpdateAndSync(100, ctx, b, cli, d1, testKey)
+			assert.NoError(b, err)
+		}
+	})
 
-		err = d2.Update(func(root *proxy.ObjectProxy) error {
-			root.Delete("k1")
-			root.SetString("k1", "v2")
-			return nil
-		}, "delete and set v2 by c2")
-		assert.NoError(b, err)
-		assert.Equal(b, `{"k1":"v2"}`, d2.Marshal())
-		syncClientsThenAssertEqual(b, []clientAndDocPair{{c1, d1}, {c2, d2}})
+	b.Run("client to client via server", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			wg := sync.WaitGroup{}
+			wg.Add(2)
+			b.StartTimer()
+			go benchmarkWatchAndUpdate(250, &wg, b)
+			go benchmarkWatchAndUpdate(250, &wg, b)
+			wg.Wait()
+		}
+	})
+
+	b.Run("adminCli to server", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			adminCli, err := admin.Dial(defaultServer.AdminAddr())
+			assert.NoError(b, err)
+			defer func() { assert.NoError(b, adminCli.Close()) }()
+
+			b.StartTimer()
+			err = benchmarkUpdateProject(1000, adminCli, b)
+			assert.NoError(b, err)
+		}
 	})
 }
