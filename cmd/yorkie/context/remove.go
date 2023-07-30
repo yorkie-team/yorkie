6fe0f6ef74db8b124b621407a127671ebf0e18cb
Wu22e <gulb1602@gmail.com>
2023-07-30 23:08:22 +0900

    add : context command

---
+++ cmd/yorkie/context/remove.go
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2022 The Yorkie Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package context
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/yorkie-team/yorkie/cmd/yorkie/config"
+)
+
+func newRemoveCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "remove [rpcAddr]",
+		Short: "Remove the context for the specified rpcAddr",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			conf, err := config.Load()
+			if err != nil {
+				return err
+			}
+
+			rpcAddr := args[0]
+
+			// Check if auth exists for the rpcAddr
+			if _, ok := conf.Auths[rpcAddr]; !ok {
+				return fmt.Errorf("no context exists for the rpcAddr: %s", rpcAddr)
+			}
+
+			// Delete the context for the rpcAddr
+			delete(conf.Auths, rpcAddr)
+
+			err = config.Save(conf)
+			if err != nil {
+				return err
+			}
+
+			fmt.Printf("Context for rpcAddr: %s has been removed.\n", rpcAddr)
+			return nil
+		},
+	}
+}
+
+func init() {
+	SubCmd.AddCommand(newRemoveCmd())
+}
---
+++ cmd/yorkie/context/set.go
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2022 The Yorkie Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package context
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	"github.com/yorkie-team/yorkie/cmd/yorkie/config"
+)
+
+func newSetContextCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "set",
+		Short: "Set the current global flags as the context",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			if err := viper.ReadInConfig(); err != nil {
+				return err
+			}
+			return nil
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			conf, err := config.Load()
+			if err != nil {
+				return err
+			}
+
+			rpcAddr := viper.GetString("rpcAddr")
+			conf.RPCAddr = rpcAddr
+			conf.IsInsecure = viper.GetBool("isInsecure")
+
+			// check if auth exists for the rpcAddr
+			if _, ok := conf.Auths[rpcAddr]; !ok {
+				conf.Auths[rpcAddr] = ""
+				fmt.Println("A new RPC address has been set. Please log in again.")
+			}
+
+			err = config.Save(conf)
+			if err != nil {
+				return err
+			}
+
+			fmt.Println("Context has been updated.")
+			return nil
+		},
+	}
+}
+
+func init() {
+	SubCmd.AddCommand(newSetContextCmd())
+}
