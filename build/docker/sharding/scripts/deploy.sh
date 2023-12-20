abd22720eec330a0ff3255faa634134c02e1b313
Sejong Kim <sepaper@naver.com>
2023-12-20 17:19:02 +0900

    Use docker compose v2 in CI

--- build/docker/sharding/scripts/deploy.sh
+++ build/docker/sharding/scripts/deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 echo "Run the Config server, Shard 1 and Shard 2"
-docker-compose -f build/docker/sharding/docker-compose.yml up --build -d --wait config1 shard1-1 shard2-1
+docker compose -f build/docker/sharding/docker-compose.yml up --build -d --wait config1 shard1-1 shard2-1
 echo $?
 echo "init config"
-docker-compose -f build/docker/sharding/docker-compose.yml exec config1 mongosh test /scripts/init-config1.js
+docker compose -f build/docker/sharding/docker-compose.yml exec config1 mongosh test /scripts/init-config1.js
 echo $?
 echo "init shard1"
-docker-compose -f build/docker/sharding/docker-compose.yml exec shard1-1 mongosh test /scripts/init-shard1-1.js
+docker compose -f build/docker/sharding/docker-compose.yml exec shard1-1 mongosh test /scripts/init-shard1-1.js
 echo $?
 echo "init shard2"
-docker-compose -f build/docker/sharding/docker-compose.yml exec shard2-1 mongosh test /scripts/init-shard2-1.js
+docker compose -f build/docker/sharding/docker-compose.yml exec shard2-1 mongosh test /scripts/init-shard2-1.js
 echo $?
 echo "Run the Mongos"
-docker-compose -f build/docker/sharding/docker-compose.yml up --build -d --wait mongos1
+docker compose -f build/docker/sharding/docker-compose.yml up --build -d --wait mongos1
 echo $?
 echo "init mongos1"
-docker-compose -f build/docker/sharding/docker-compose.yml exec mongos1 mongosh test /scripts/init-mongos1.js
+docker compose -f build/docker/sharding/docker-compose.yml exec mongos1 mongosh test /scripts/init-mongos1.js
 echo $?
