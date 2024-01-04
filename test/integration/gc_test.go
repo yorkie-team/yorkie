84dee981facdbf8b7d045f2c56c98dd801dda9e7
JongBeom Lee <jongbeom.lee@navercorp.com>
2024-01-04 10:57:34 +0900

    Add test case for garbage collection with overlapped removal

--- test/integration/gc_test.go
+++ test/integration/gc_test.go
@@ -382,4 +382,97 @@ func TestGarbageCollection(t *testing.T) {
 		assert.Equal(t, 0, d1.GarbageLen())
 		assert.Equal(t, 6, d2.GarbageLen())
 	})
+
+	t.Run("garbage collection with overlapped removal", func(t *testing.T) {
+		ctx := context.Background()
+		docKey := helper.TestDocKey(t)
+		d1 := document.New(docKey)
+		d2 := document.New(docKey)
+
+		err := c1.Attach(ctx, d1)
+		assert.NoError(t, err)
+
+		err = d1.Update(func(root *json.Object, p *presence.Presence) error {
+			root.SetNewObject("point").
+				SetInteger("point.x", 0)
+			return nil
+		})
+		err = c1.Sync(ctx)
+		assert.NoError(t, err)
+		err = c2.Attach(ctx, d2)
+		assert.NoError(t, err)
+
+		err = d1.Update(func(root *json.Object, p *presence.Presence) error {
+			root.Delete("point")
+			return nil
+		})
+		assert.Equal(t, 2, d1.GarbageLen())
+
+		err = d2.Update(func(root *json.Object, p *presence.Presence) error {
+			root.GetObject("point").Delete("point.x")
+			return nil
+		})
+		assert.NoError(t, err)
+
+		err = c1.Sync(ctx)
+		assert.NoError(t, err)
+		err = c2.Sync(ctx)
+		assert.NoError(t, err)
+		err = c1.Sync(ctx)
+		assert.NoError(t, err)
+
+		assert.Equal(t, 2, d1.GarbageLen())
+		assert.Equal(t, 2, d2.GarbageLen())
+
+		assert.Equal(t, d1.GarbageCollect(time.MaxTicket), 2)
+		assert.Equal(t, d2.GarbageCollect(time.MaxTicket), 2)
+	})
+
+	t.Run("garbage collection with overlapped removal 2", func(t *testing.T) {
+		ctx := context.Background()
+		docKey := helper.TestDocKey(t)
+		d1 := document.New(docKey)
+		d2 := document.New(docKey)
+
+		err := c1.Attach(ctx, d1)
+		assert.NoError(t, err)
+
+		err = d1.Update(func(root *json.Object, p *presence.Presence) error {
+			root.SetNewObject("point").
+				SetInteger("point.x", 0).
+				SetInteger("point.y", 0)
+			return nil
+		})
+		err = c1.Sync(ctx)
+		assert.NoError(t, err)
+		err = c2.Attach(ctx, d2)
+		assert.NoError(t, err)
+
+		err = d1.Update(func(root *json.Object, p *presence.Presence) error {
+			root.Delete("point")
+			return nil
+		})
+		assert.NoError(t, err)
+		assert.Equal(t, 3, d1.GarbageLen())
+
+		err = d2.Update(func(root *json.Object, p *presence.Presence) error {
+			root.GetObject("point").Delete("point.x")
+			return nil
+		})
+		assert.NoError(t, err)
+		assert.Equal(t, 1, d2.GarbageLen())
+
+		err = c1.Sync(ctx)
+		assert.NoError(t, err)
+		err = c2.Sync(ctx)
+		assert.NoError(t, err)
+		err = c1.Sync(ctx)
+		assert.NoError(t, err)
+
+		assert.Equal(t, 3, d1.GarbageLen())
+		assert.Equal(t, 3, d2.GarbageLen())
+
+		assert.Equal(t, d1.GarbageCollect(time.MaxTicket), 3)
+		assert.Equal(t, d2.GarbageCollect(time.MaxTicket), 3)
+	})
 }
