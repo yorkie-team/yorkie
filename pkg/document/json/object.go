652540c08371c71310e9daeda40ad38d3b9d7ab8
highcloud100 <highcloud.100@navercorp.com>
2024-01-17 14:22:00 +0900

    Refactor array test, newObject

--- pkg/document/json/object.go
+++ pkg/document/json/object.go
@@ -43,10 +43,6 @@ func NewObject(ctx *change.Context, root *crdt.Object) *Object {
 
 // SetNewObject sets a new Object for the given key.
 func (p *Object) SetNewObject(k string, v ...map[string]interface{}) *Object {
-	if len(v) > 1 {
-		panic("too many arguments in call to SetNewObject")
-	}
-
 	value := p.setInternal(k, func(ticket *time.Ticket) crdt.Element {
 		if len(v) == 0 {
 			return NewObject(p.context, crdt.NewObject(crdt.NewElementRHT(), ticket))
