ee37c3b72f5416ca98f2666297e60fad6a1d080d
highcloud100 <highcloud.100@navercorp.com>
2024-01-23 11:19:57 +0900

    Add default value of counter

--- pkg/document/crdt/counter.go
+++ pkg/document/crdt/counter.go
@@ -60,6 +60,9 @@ type Counter struct {
 
 // NewCounter creates a new instance of Counter.
 func NewCounter(valueType CounterType, value interface{}, createdAt *time.Ticket) (*Counter, error) {
+	if value == nil {
+		value = 0
+	}
 	switch valueType {
 	case IntegerCnt:
 		intValue, err := castToInt(value)
