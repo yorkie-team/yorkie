0bfba1f0d602054161a84f22a3840b6fa82af067
taeng0204 <taeng02@icloud.com>
2024-08-06 12:28:45 +0900

    Add a service-specific health check test
    Write a health check for each service and a check failure test for the unknown service.

--- test/integration/health_test.go
+++ test/integration/health_test.go
@@ -26,6 +26,7 @@ import (
 
 	"connectrpc.com/grpchealth"
 	"github.com/stretchr/testify/assert"
+	"github.com/yorkie-team/yorkie/api/yorkie/v1/v1connect"
 	"github.com/yorkie-team/yorkie/server/rpc/health"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -49,6 +50,81 @@ func TestRPCHealthCheck(t *testing.T) {
 	assert.Equal(t, resp.Status, healthpb.HealthCheckResponse_SERVING)
 }
 
+func TestRPCHealthCheckYorkie(t *testing.T) {
+	// use gRPC health check for Yorkie
+	conn, err := grpc.Dial(
+		defaultServer.RPCAddr(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	assert.NoError(t, err)
+	defer func() {
+		assert.NoError(t, conn.Close())
+	}()
+
+	cli := healthpb.NewHealthClient(conn)
+	resp, err := cli.Check(context.Background(), &healthpb.HealthCheckRequest{
+		Service: v1connect.YorkieServiceName,
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, resp.Status, healthpb.HealthCheckResponse_SERVING)
+}
+
+func TestRPCHealthCheckAdmin(t *testing.T) {
+	// use gRPC health check for Admin
+	conn, err := grpc.Dial(
+		defaultServer.RPCAddr(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	assert.NoError(t, err)
+	defer func() {
+		assert.NoError(t, conn.Close())
+	}()
+
+	cli := healthpb.NewHealthClient(conn)
+	resp, err := cli.Check(context.Background(), &healthpb.HealthCheckRequest{
+		Service: v1connect.AdminServiceName,
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, resp.Status, healthpb.HealthCheckResponse_SERVING)
+}
+
+func TestRPCHealthCheckHealthService(t *testing.T) {
+	// use gRPC health check for health service
+	conn, err := grpc.Dial(
+		defaultServer.RPCAddr(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	assert.NoError(t, err)
+	defer func() {
+		assert.NoError(t, conn.Close())
+	}()
+
+	cli := healthpb.NewHealthClient(conn)
+	resp, err := cli.Check(context.Background(), &healthpb.HealthCheckRequest{
+		Service: grpchealth.HealthV1ServiceName,
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, resp.Status, healthpb.HealthCheckResponse_SERVING)
+}
+
+func TestRPCHealthCheckUnknownService(t *testing.T) {
+	// use gRPC health check for unknown service
+	conn, err := grpc.Dial(
+		defaultServer.RPCAddr(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	assert.NoError(t, err)
+	defer func() {
+		assert.NoError(t, conn.Close())
+	}()
+
+	cli := healthpb.NewHealthClient(conn)
+	_, err = cli.Check(context.Background(), &healthpb.HealthCheckRequest{
+		Service: "unknown",
+	})
+	assert.Error(t, err)
+}
+
 func TestHTTPHealthCheck(t *testing.T) {
 	// use HTTP health check
 	resp, err := http.Get("http://" + defaultServer.RPCAddr() + "/healthz/")
@@ -62,4 +138,59 @@ func TestHTTPHealthCheck(t *testing.T) {
 	err = json.NewDecoder(resp.Body).Decode(&healthResp)
 	assert.NoError(t, err)
 	assert.Equal(t, healthResp.Status, grpchealth.StatusServing.String())
+}
+
+func TestHTTPHealthCheckYorkie(t *testing.T) {
+	// use HTTP health check for Yorkie
+	resp, err := http.Get("http://" + defaultServer.RPCAddr() + "/healthz/?service=" + v1connect.YorkieServiceName)
+	defer func() {
+		assert.NoError(t, resp.Body.Close())
+	}()
+	assert.NoError(t, err)
+	assert.Equal(t, resp.StatusCode, 200)
+
+	var healthResp health.CheckResponse
+	err = json.NewDecoder(resp.Body).Decode(&healthResp)
+	assert.NoError(t, err)
+	assert.Equal(t, healthResp.Status, grpchealth.StatusServing.String())
+}
+
+func TestHTTPHealthCheckAdmin(t *testing.T) {
+	// use HTTP health check for Admin
+	resp, err := http.Get("http://" + defaultServer.RPCAddr() + "/healthz/?service=" + v1connect.AdminServiceName)
+	defer func() {
+		assert.NoError(t, resp.Body.Close())
+	}()
+	assert.NoError(t, err)
+	assert.Equal(t, resp.StatusCode, 200)
+
+	var healthResp health.CheckResponse
+	err = json.NewDecoder(resp.Body).Decode(&healthResp)
+	assert.NoError(t, err)
+	assert.Equal(t, healthResp.Status, grpchealth.StatusServing.String())
+}
+
+func TestHTTPHealthCheckHealthService(t *testing.T) {
+	// use HTTP health check for health service
+	resp, err := http.Get("http://" + defaultServer.RPCAddr() + "/healthz/?service=" + grpchealth.HealthV1ServiceName)
+	defer func() {
+		assert.NoError(t, resp.Body.Close())
+	}()
+	assert.NoError(t, err)
+	assert.Equal(t, resp.StatusCode, 200)
+
+	var healthResp health.CheckResponse
+	err = json.NewDecoder(resp.Body).Decode(&healthResp)
+	assert.NoError(t, err)
+	assert.Equal(t, healthResp.Status, grpchealth.StatusServing.String())
+}
+
+func TestHTTPHealthCheckUnknownService(t *testing.T) {
+	// use HTTP health check for unknown service
+	resp, err := http.Get("http://" + defaultServer.RPCAddr() + "/healthz/?service=unknown")
+	defer func() {
+		assert.NoError(t, resp.Body.Close())
+	}()
+	assert.NoError(t, err)
+	assert.Equal(t, resp.StatusCode, 404)
 }
