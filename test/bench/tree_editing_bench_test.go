64d8e6f023dc02fab51e493d364ebdf5c8ab4a3e
m4ushold <m4ushold@gmail.com>
2024-09-16 21:57:11 +0900

    Add splay tree Benchmark test
    Add benchmark tests to see how much performance optimization improves
    the splay tree.

--- test/bench/tree_editing_bench_test.go
+++ test/bench/tree_editing_bench_test.go
@@ -20,15 +20,121 @@ package bench
 
 import (
 	"fmt"
+	"math/rand"
+	"strconv"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/yorkie-team/yorkie/api/converter"
 	"github.com/yorkie-team/yorkie/pkg/document/crdt"
 	"github.com/yorkie-team/yorkie/pkg/document/json"
+	"github.com/yorkie-team/yorkie/pkg/splay"
 	"github.com/yorkie-team/yorkie/test/helper"
 )
+
+func randomArray(n, min, max int) []int {
+	arr := make([]int, n)
+	rand.Seed(time.Now().UnixNano())
+
+	for i := 0; i < n; i++ {
+		arr[i] = rand.Intn(max-min+1) + min
+	}
+
+	return arr
+}
+
+type stringValue struct {
+	content string
+	removed bool
+}
+
+func newSplayNode(content string) *splay.Node[*stringValue] {
+	return splay.NewNode(&stringValue{
+		content: content,
+	})
+}
+
+func (v *stringValue) Len() int {
+	if v.removed {
+		return 0
+	}
+	return len(v.content)
+}
+
+func (v *stringValue) String() string {
+	return v.content
+}
+
+func BenchmarkSplayTree(b *testing.B) {
+	operationCount := []int{10000, 20000, 30000}
+
+	for _, n := range operationCount {
+		b.ResetTimer()
+		arr := randomArray(n, 1, n)
+
+		b.Run(fmt.Sprintf("Insert %d", n), func(b *testing.B) {
+			tree := splay.NewTree[*stringValue](nil)
+			for _, i := range arr {
+				tree.Insert(newSplayNode(strconv.FormatInt(int64(i), 10)))
+			}
+		})
+		b.Run(fmt.Sprintf("Random read %d", n), func(b *testing.B) {
+			b.StopTimer()
+			tree := splay.NewTree[*stringValue](nil)
+			for _, i := range arr {
+				tree.Insert(newSplayNode(strconv.FormatInt(int64(i), 10)))
+			}
+			b.StartTimer()
+			for _, i := range randomArray(n, 0, n-1) {
+				tree.Find(i)
+			}
+		})
+		b.Run(fmt.Sprintf("Delete %d", n), func(b *testing.B) {
+			b.StopTimer()
+			tree := splay.NewTree[*stringValue](nil)
+			for _, i := range arr {
+				tree.Insert(newSplayNode(strconv.FormatInt(int64(i), 10)))
+			}
+			b.StartTimer()
+			for _, i := range randomArray(n, 1, n) {
+				node, _, _ := tree.Find(i)
+				if node != nil {
+					tree.Delete(node)
+				}
+			}
+		})
+	}
+
+	b.Run("edit splay tree", func(b *testing.B) {
+		b.StopTimer()
+
+		editingTrace, err := readEditingTraceFromFile(b)
+		if err != nil {
+			b.Fatal(err)
+		}
+
+		b.StartTimer()
+		tree := splay.NewTree[*stringValue](nil)
+		for _, edit := range editingTrace.Edits {
+			cursor := int(edit[0].(float64))
+			mode := int(edit[1].(float64))
+
+			if mode == 0 {
+				strValue, ok := edit[2].(string)
+				if ok {
+					tree.Insert(newSplayNode(strValue))
+				}
+			} else {
+				node, _, err := tree.Find(cursor)
+				if err != nil && node != nil {
+					tree.Delete(node)
+				}
+			}
+		}
+	})
+}
 
 func BenchmarkTree(b *testing.B) {
 	verticesCounts := []int{10000, 20000, 30000}
