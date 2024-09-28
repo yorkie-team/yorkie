f619182b7dae81efb298d8ba586b4da92d79b365
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-09-28 15:15:08 +0900

    Implement output format of command, `context ls`

--- cmd/yorkie/context/list.go
+++ cmd/yorkie/context/list.go
@@ -17,14 +17,24 @@
 package context
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
-
 	"github.com/jedib0t/go-pretty/v6/table"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"gopkg.in/yaml.v3"
 
 	"github.com/yorkie-team/yorkie/cmd/yorkie/config"
 )
+
+type Info struct {
+	Current  string `json:"current" yaml:"current"`
+	RPCAddr  string `json:"rpc_addr" yaml:"rpc_addr"`
+	Insecure string `json:"insecure" yaml:"insecure"`
+	Token    string `json:"token" yaml:"token"`
+}
 
 func newListCommand() *cobra.Command {
 	return &cobra.Command{
@@ -37,14 +47,12 @@ func newListCommand() *cobra.Command {
 				return err
 			}
 
-			tw := table.NewWriter()
-			tw.Style().Options.DrawBorder = false
-			tw.Style().Options.SeparateColumns = false
-			tw.Style().Options.SeparateFooter = false
-			tw.Style().Options.SeparateHeader = false
-			tw.Style().Options.SeparateRows = false
+			output := viper.GetString("output")
+			if err := validateOutputOpts(output); err != nil {
+				return err
+			}
 
-			tw.AppendHeader(table.Row{"CURRENT", "RPC ADDR", "INSECURE", "TOKEN"})
+			contexts := make([]Info, 0, len(conf.Auths))
 			for rpcAddr, auth := range conf.Auths {
 				current := ""
 				if rpcAddr == viper.GetString("rpcAddr") {
@@ -61,14 +69,61 @@ func newListCommand() *cobra.Command {
 					ellipsisToken = auth.Token[:10] + "..." + auth.Token[len(auth.Token)-10:]
 				}
 
-				tw.AppendRow(table.Row{current, rpcAddr, insecure, ellipsisToken})
+				contexts = append(contexts, Info{
+					Current:  current,
+					RPCAddr:  rpcAddr,
+					Insecure: insecure,
+					Token:    ellipsisToken,
+				})
 			}
 
-			fmt.Println(tw.Render())
+			if err := printContexts(cmd, output, contexts); err != nil {
+				return err
+			}
 
 			return nil
 		},
 	}
+}
+
+func printContexts(cmd *cobra.Command, outputFormat string, contexts []Info) error {
+	switch outputFormat {
+	case "":
+		tw := table.NewWriter()
+		tw.Style().Options.DrawBorder = false
+		tw.Style().Options.SeparateColumns = false
+		tw.Style().Options.SeparateFooter = false
+		tw.Style().Options.SeparateHeader = false
+		tw.Style().Options.SeparateRows = false
+
+		tw.AppendHeader(table.Row{"CURRENT", "RPC ADDR", "INSECURE", "TOKEN"})
+		for _, ctx := range contexts {
+			tw.AppendRow(table.Row{ctx.Current, ctx.RPCAddr, ctx.Insecure, ctx.Token})
+		}
+		fmt.Println(tw.Render())
+	case "json":
+		marshalled, err := json.MarshalIndent(contexts, "", "  ")
+		if err != nil {
+			return errors.New("marshal JSON")
+		}
+		cmd.Println(string(marshalled))
+	case "yaml":
+		marshalled, err := yaml.Marshal(contexts)
+		if err != nil {
+			return errors.New("marshal YAML")
+		}
+		fmt.Println(string(marshalled))
+	default:
+		return errors.New("unknown output format")
+	}
+	return nil
+}
+
+func validateOutputOpts(output string) error {
+	if output != "" && output != "yaml" && output != "json" {
+		return errors.New(`--output must be 'yaml' or 'json'`)
+	}
+	return nil
 }
 
 func init() {
