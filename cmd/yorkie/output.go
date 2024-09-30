77e24f8db6374c2a8e8d7a2202bc9528c81268af
Taeyeon <90550065+TaeyeonRoyce@users.noreply.github.com>
2024-09-30 19:46:01 +0900

    Provide global output flag for all cli commands (#1021)
    Provide a consistent way for users to set the output format globally
    across all CLI commands. Implement JSON and YAML options for the
    output flag. Update validation for user input options to be global.
    Apply new output formats to: context ls, project create/ls/update,
    document ls, and history ls commands.

---
+++ cmd/yorkie/output.go
@@ -0,0 +1,7 @@
+package main
+
+const (
+	DefaultOutput = ""     // DefaultOutput is for table format
+	YamlOutput    = "yaml" // YamlOutput is for yaml format
+	JSONOutput    = "json" // JSONOutput is for json format
+)
