5482729415e18faa6d6754e8fa34b8549999f1f0
raararaara <raararaara@gmail.com>
2024-11-11 16:12:25 +0900

    Add detach documents script

--- migrations/v0.5.6/document-detach.go
+++ migrations/v0.5.6/document-detach.go
@@ -19,17 +19,95 @@ package v056
 import (
 	"context"
 	"fmt"
-	"github.com/yorkie-team/yorkie/server/backend/database"
+	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+
+	"github.com/yorkie-team/yorkie/api/types"
+	"github.com/yorkie-team/yorkie/server/backend/database"
 )
 
+const (
+	// StatusKey is the key of the status field.
+	StatusKey = "status"
+)
+
+func validateDetach(ctx context.Context, collection *mongo.Collection) ([]string, error) {
+	var failedClients []string
+	totalCount, err := collection.CountDocuments(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	fmt.Printf("Validation check for %d clients\n", totalCount)
+
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+
+	for cursor.Next(ctx) {
+		var info database.ClientInfo
+		if err := cursor.Decode(&info); err != nil {
+			return nil, fmt.Errorf("decode client info: %w", err)
+		}
+
+		// 01. ensure deactivated
+		if info.Status != database.ClientDeactivated {
+			continue
+		}
+
+		// 02. ensure detached
+		hasAttachedDocs := false
+		for _, clientDocInfo := range info.Documents {
+			if clientDocInfo.Status == database.DocumentAttached {
+				hasAttachedDocs = true
+				break
+			}
+		}
+		if hasAttachedDocs {
+			failedClients = append(failedClients, info.ID.String())
+		}
+	}
+
+	return failedClients, nil
+}
+
 func progressMigrationBach(
-	_ context.Context,
-	_ *mongo.Collection,
-	_ []database.ClientInfo,
+	ctx context.Context,
+	collection *mongo.Collection,
+	infos []*database.ClientInfo,
 ) error {
+	for _, info := range infos {
+		fmt.Printf("clientID: %s\n", info.ID)
+		for docID, clientDocInfo := range info.Documents {
+			if clientDocInfo.Status != database.DocumentAttached {
+				continue
+			}
+
+			updater := bson.M{
+				"$set": bson.M{
+					clientDocInfoKey(docID, "server_seq"): 0,
+					clientDocInfoKey(docID, "client_seq"): 0,
+					clientDocInfoKey(docID, StatusKey):    database.DocumentDetached,
+					"updated_at":                          time.Now(),
+				},
+			}
+
+			result := collection.FindOneAndUpdate(ctx, bson.M{
+				"project_id": info.ProjectID,
+				"_id":        info.ID,
+			}, updater)
+
+			if result.Err() != nil {
+				if result.Err() == mongo.ErrNoDocuments {
+					return fmt.Errorf("%s: %w", info.Key, database.ErrClientNotFound)
+				}
+				return fmt.Errorf("update client info: %w", result.Err())
+			}
+		}
+
+	}
 	return nil
 }
 
@@ -76,27 +154,12 @@ func DetachDocumentsFromDeactivatedClients(
 	}
 	fmt.Printf("total clients: %d\n", totalCount)
 
-	//res := collection.FindOne(ctx, filter)
-	//if res.Err() == mongo.ErrNoDocuments {
-	//	return fmt.Errorf("no doc")
-	//}
-	//if res.Err() != nil {
-	//	return fmt.Errorf("error occurs")
-	//}
-	//
-	//clientInfo := database.ClientInfo{}
-	//if err := res.Decode(&clientInfo); err != nil {
-	//	return fmt.Errorf("decode error")
-	//}
-	//
-	//fmt.Printf("%d %s\n", batchSize, clientInfo.ID.String())
-
 	cursor, err := collection.Find(ctx, filter)
 	if err != nil {
 		return err
 	}
 
-	var infos []database.ClientInfo
+	var infos []*database.ClientInfo
 
 	batchCount := 1
 	for cursor.Next(ctx) {
@@ -105,7 +168,7 @@ func DetachDocumentsFromDeactivatedClients(
 			return fmt.Errorf("decode client info: %w", err)
 		}
 
-		infos = append(infos, clientInfo)
+		infos = append(infos, &clientInfo)
 
 		if len(infos) >= batchSize {
 			if err := progressMigrationBach(ctx, collection, infos); err != nil {
@@ -123,7 +186,19 @@ func DetachDocumentsFromDeactivatedClients(
 		}
 	}
 
-	// TODO(raararaara): validation check
+	res, err := validateDetach(ctx, collection)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Number of failed clients: %d\n", len(res))
+	if 0 < len(res) && len(res) < 100 {
+		fmt.Print(res)
+	}
 
 	return nil
+}
+
+// clientDocInfoKey returns the key for the client document info.
+func clientDocInfoKey(docID types.ID, prefix string) string {
+	return fmt.Sprintf("documents.%s.%s", docID, prefix)
 }
