e95dbfa882b38b358ba264b4c5c85fd2a0f5b8fa
Yourim Cha <yourim.cha@navercorp.com>
2025-02-20 16:44:46 +0900

    Test benchmark ci

--- test/bench/vv_bench_test.go
+++ test/bench/vv_bench_test.go
@@ -200,7 +200,7 @@ func benchmarkVV(
 func BenchmarkVersionVector(b *testing.B) {
 	err := logging.SetLogLevel("error")
 	assert.NoError(b, err)
-	startTestServer(100000, 100000)
+	startTestServer(10, 5)
 	defer func() {
 		if testServer == nil {
 			return
