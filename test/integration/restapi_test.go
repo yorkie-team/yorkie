yorkie-team/yorkie

commit 3b3283da401b936233c5b7d194d3a3251a24e179

Optimize snapshot handling with in-memory cache (#1280)
When a new client attaches to a document and the number of changes
exceeds the SnapshotThreshold, a snapshot is now returned. To reduce
frequent database queries caused by repeated snapshot building, the
most recently built snapshot is now cached in memory.

Additionally, tombstones are now removed when creating a snapshot,
reducing the payload size.

pkg/document/internal_document.go
@@ -419,3 +419,22 @@ func (d *InternalDocument) ToDocument() *Document {
 	doc.setInternalDoc(d)
 	return doc
 }
+
+// DeepCopy creates a deep copy of this document.
+func (d *InternalDocument) DeepCopy() (*InternalDocument, error) {
+	root, err := d.root.DeepCopy()
+	if err != nil {
+		return nil, err
+	}
+
+	return &InternalDocument{
+		key:           d.key,
+		status:        d.status,
+		checkpoint:    d.checkpoint,
+		changeID:      d.changeID,
+		root:          root,
+		presences:     d.presences.DeepCopy(),
+		onlineClients: &gosync.Map{},
+		localChanges:  d.localChanges,
+	}, nil
+}

server/packs/packs.go
@@ -261,34 +261,40 @@ func BuildInternalDocForServerSeq(
 	serverSeq int64,
 ) (*document.InternalDocument, error) {
 	docRefKey := docInfo.RefKey()
-	snapshotInfo, err := be.DB.FindClosestSnapshotInfo(
-		ctx,
-		docRefKey,
-		serverSeq,
-		true,
-	)
-	if err != nil {
-		return nil, err
+
+	// NOTE(hackerwins): If the document is already in the cache, we can skip
+	// the database query and use the cached document. If the document's server
+	// sequence in the cache is greater than the given server sequence, we can't
+	// build the document from the document. In this case, we need to
+	// query the database to get the closest snapshot information.
+	doc, ok := be.SnapshotCache.Get(docRefKey)
+	if !ok || serverSeq < doc.Checkpoint().ServerSeq {
+		snapshotInfo, err := be.DB.FindClosestSnapshotInfo(
+			ctx,
+			docRefKey,
+			serverSeq,
+			true,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		doc, err = document.NewInternalDocumentFromSnapshot(
+			docInfo.Key,
+			snapshotInfo.ServerSeq,
+			snapshotInfo.Lamport,
+			snapshotInfo.VersionVector,
+			snapshotInfo.Snapshot,
+		)
+		if err != nil {
+			return nil, err
+		}
 	}
 
-	doc, err := document.NewInternalDocumentFromSnapshot(
-		docInfo.Key,
-		snapshotInfo.ServerSeq,
-		snapshotInfo.Lamport,
-		snapshotInfo.VersionVector,
-		snapshotInfo.Snapshot,
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	// TODO(hackerwins): If the Snapshot is missing, we may have a very large
-	// number of changes to read at once here. We need to split changes by a
-	// certain size (e.g. 100) and read and gradually reflect it into the document.
 	changes, err := be.DB.FindChangesBetweenServerSeqs(
 		ctx,
 		docRefKey,
-		snapshotInfo.ServerSeq+1,
+		doc.Checkpoint().ServerSeq+1,
 		serverSeq,
 	)
 	if err != nil {
@@ -304,6 +310,27 @@ func BuildInternalDocForServerSeq(
 	), be.Config.SnapshotDisableGC); err != nil {
 		return nil, err
 	}
+
+	if !be.Config.SnapshotDisableGC {
+		vector, err := be.DB.GetMinVersionVector(
+			ctx,
+			docRefKey,
+			doc.VersionVector(),
+		)
+		if err != nil {
+			return nil, err
+		}
+		if _, err := doc.GarbageCollect(vector); err != nil {
+			return nil, err
+		}
+	}
+
+	// NOTE(hackerwins): Store the latest document in the cache.
+	clone, err := doc.DeepCopy()
+	if err != nil {
+		return nil, err
+	}
+	be.SnapshotCache.Add(docRefKey, clone)
 
 	if logging.Enabled(zap.DebugLevel) {
 		logging.From(ctx).Debugf(

cmd/yorkie/server.go
@@ -399,6 +399,12 @@ func init() {
 		server.DefaultSnapshotDisableGC,
 		"Whether to disable garbage collection of snapshots.",
 	)
+	cmd.Flags().IntVar(
+		&conf.Backend.SnapshotCacheSize,
+		"snapshot-cache-size",
+		server.DefaultSnapshotCacheSize,
+		"The cache size of the snapshots.",
+	)
 	cmd.Flags().DurationVar(
 		&authWebhookRequestTimeout,
 		"auth-webhook-request-timeout",

server/backend/backend.go
@@ -24,10 +24,12 @@ import (
 	"fmt"
 	"os"
 
+	lru "github.com/hashicorp/golang-lru/v2"
 	"github.com/hashicorp/golang-lru/v2/expirable"
 
 	"github.com/yorkie-team/yorkie/api/types"
 	"github.com/yorkie-team/yorkie/cluster"
+	"github.com/yorkie-team/yorkie/pkg/document"
 	pkgtypes "github.com/yorkie-team/yorkie/pkg/types"
 	pkgwebhook "github.com/yorkie-team/yorkie/pkg/webhook"
 	"github.com/yorkie-team/yorkie/server/backend/background"
@@ -53,6 +55,8 @@ type Backend struct {
 	AuthWebhookCache *expirable.LRU[string, pkgtypes.Pair[int, *types.AuthWebhookResponse]]
 	// AuthWebhookClient is used to send auth webhook.
 	AuthWebhookClient *pkgwebhook.Client[types.AuthWebhookRequest, types.AuthWebhookResponse]
+	// SnapshotCache is used to cache the snapshot information.
+	SnapshotCache *lru.Cache[types.DocRefKey, *document.InternalDocument]
 
 	// ClusterClient is used to send requests to nodes in the cluster.
 	ClusterClient *cluster.Client
@@ -100,7 +104,7 @@ func New(
 		conf.Hostname = hostname
 	}
 
-	// 02. Create the webhook authWebhookCache and client.
+	// 02. Create webhook clients and its cache.
 	authWebhookCache := expirable.NewLRU[string, pkgtypes.Pair[int, *types.AuthWebhookResponse]](
 		conf.AuthWebhookCacheSize,
 		nil,
@@ -114,7 +118,6 @@ func New(
 			RequestTimeout:  conf.ParseAuthWebhookRequestTimeout(),
 		},
 	)
-
 	eventWebhookManger := webhook.NewManager(pkgwebhook.NewClient[types.EventWebhookRequest, int](
 		pkgwebhook.Options{
 			MaxRetries:      conf.EventWebhookMaxRetries,
@@ -123,6 +126,13 @@ func New(
 			RequestTimeout:  conf.ParseEventWebhookRequestTimeout(),
 		},
 	))
+
+	snapshotCache, err := lru.New[types.DocRefKey, *document.InternalDocument](
+		conf.SnapshotCacheSize,
+	)
+	if err != nil {
+		return nil, err
+	}
 
 	// 03. Create the cluster client. The cluster client is used to send
 	// requests to other nodes in the cluster.
@@ -201,6 +211,8 @@ func New(
 		AuthWebhookCache:    authWebhookCache,
 		AuthWebhookClient:   authWebhookClient,
 		EventWebhookManager: eventWebhookManger,
+
+		SnapshotCache: snapshotCache,
 
 		ClusterClient: clusterClient,
 

server/backend/config.go
@@ -73,6 +73,9 @@ type Config struct {
 	// AuthWebhookCacheTTL is the TTL value to set when caching the authorized result.
 	AuthWebhookCacheTTL string `yaml:"AuthWebhookCacheTTL"`
 
+	// SnapshotCacheSize is the cache size of the snapshot.
+	SnapshotCacheSize int `yaml:"SnapshotCacheSize"`
+
 	// EventWebhookMaxRetries is the max count that retries the event webhook.
 	EventWebhookMaxRetries uint64 `yaml:"EventWebhookMaxRetries"`
 

server/backend/database/database.go
@@ -295,7 +295,14 @@ type Database interface {
 		ctx context.Context,
 		clientInfo *ClientInfo,
 		docRefKey types.DocRefKey,
-		versionVector time.VersionVector,
+		vector time.VersionVector,
+	) (time.VersionVector, error)
+
+	// GetMinVersionVector returns the minimum version vector of all clients.
+	GetMinVersionVector(
+		ctx context.Context,
+		docRefKey types.DocRefKey,
+		vector time.VersionVector,
 	) (time.VersionVector, error)
 
 	// FindDocInfosByPaging returns the documentInfos of the given paging.

server/backend/database/memory/database.go
@@ -1449,12 +1449,25 @@ func (d *DB) UpdateMinVersionVector(
 	ctx context.Context,
 	clientInfo *database.ClientInfo,
 	docRefKey types.DocRefKey,
-	versionVector time.VersionVector,
+	vector time.VersionVector,
 ) (time.VersionVector, error) {
+	// 01. Update synced version vector of the given client and document.
 	// TODO(JOOHOJANG): We have to consider removing detached client's lamport
 	// from min version vector.
+	if err := d.updateVersionVector(ctx, clientInfo, docRefKey, vector); err != nil {
+		return nil, err
+	}
 
-	// 01. Find all version vectors of the given document from DB.
+	// 02. Compute min version vector.
+	return d.GetMinVersionVector(ctx, docRefKey, vector)
+}
+
+// GetMinVersionVector returns the minimum version vector of the given document.
+func (d *DB) GetMinVersionVector(
+	_ context.Context,
+	docRefKey types.DocRefKey,
+	vector time.VersionVector,
+) (time.VersionVector, error) {
 	txn := d.db.Txn(false)
 	defer txn.Abort()
 	iterator, err := txn.Get(tblVersionVectors, "doc_id", docRefKey.DocID.String())
@@ -1462,28 +1475,19 @@ func (d *DB) UpdateMinVersionVector(
 		return nil, fmt.Errorf("find all version vectors: %w", err)
 	}
 
-	var versionVectorInfos []database.VersionVectorInfo
+	var infos []database.VersionVectorInfo
 	for raw := iterator.Next(); raw != nil; raw = iterator.Next() {
 		vvi := raw.(*database.VersionVectorInfo)
-		versionVectorInfos = append(versionVectorInfos, *vvi)
+		infos = append(infos, *vvi)
 	}
 
-	// 02. Compute min version vector.
-	minVersionVector := versionVector.DeepCopy()
-	for i, vvi := range versionVectorInfos {
-		if vvi.ClientID == clientInfo.ID {
-			continue
-		}
-		minVersionVector.Min(&versionVectorInfos[i].VersionVector)
+	var vectors []time.VersionVector
+	vectors = append(vectors, vector)
+	for _, vv := range infos {
+		vectors = append(vectors, vv.VersionVector)
 	}
 
-	// 03. Update current client's version vector. If the client is detached, remove it.
-	// This is only for the current client and does not affect the version vector of other clients.
-	if err = d.updateVersionVector(ctx, clientInfo, docRefKey, versionVector); err != nil {
-		return nil, err
-	}
-
-	return minVersionVector, nil
+	return time.MinVersionVector(vectors...), nil
 }
 
 // FindDocInfosByPaging returns the documentInfos of the given paging.

server/backend/database/mongo/client.go
@@ -1348,6 +1348,8 @@ func (c *Client) UpdateMinVersionVector(
 	vector time.VersionVector,
 ) (time.VersionVector, error) {
 	// 01. Update synced version vector of the given client and document.
+	// NOTE(hackerwins): Considering removing the detached client's lamport
+	// from the other clients' version vectors. For now, we just ignore it.
 	if err := c.updateVersionVector(ctx, clientInfo, docRefKey, vector); err != nil {
 		return nil, err
 	}
@@ -1365,8 +1367,6 @@ func (c *Client) UpdateMinVersionVector(
 				return vector
 			})
 		} else {
-			// NOTE(hackerwins): Considering removing the detached client's lamport
-			// from the other clients' version vectors. For now, we just ignore it.
 			vvMap.Delete(clientInfo.ID, func(value time.VersionVector, exists bool) bool {
 				return exists
 			})
@@ -1374,6 +1374,15 @@ func (c *Client) UpdateMinVersionVector(
 	}
 
 	// 03. Calculate the minimum version vector of the given document.
+	return c.GetMinVersionVector(ctx, docRefKey, vector)
+}
+
+// GetMinVersionVector returns the minimum version vector of the given document.
+func (c *Client) GetMinVersionVector(
+	ctx context.Context,
+	docRefKey types.DocRefKey,
+	vector time.VersionVector,
+) (time.VersionVector, error) {
 	if !c.vvCache.Contains(docRefKey) {
 		var infos []database.VersionVectorInfo
 		cursor, err := c.collection(ColVersionVectors).Find(ctx, bson.M{

commit 67d5e78d031293f9c03bcdf3e4d51aa572149d78

Fix invalid snapshot caching logic (#1291)
Addressed concurrency issues in the snapshot cache introduced in #1280.
This fix ensures stable and correct snapshot usage by cloning snapshots
before reuse, avoiding shared-state side effects.

pkg/document/change/id.go
@@ -192,3 +192,14 @@ func (id ID) VersionVector() time.VersionVector {
 func (id ID) AfterOrEqual(other ID) bool {
 	return id.versionVector.AfterOrEqual(other.versionVector)
 }
+
+// DeepCopy clones the ID deeply.
+func (id ID) DeepCopy() ID {
+	return NewID(
+		id.clientSeq,
+		id.serverSeq,
+		id.lamport,
+		id.actorID,
+		id.versionVector.DeepCopy(),
+	)
+}

pkg/document/internal_document.go
@@ -428,10 +428,15 @@ func (d *InternalDocument) DeepCopy() (*InternalDocument, error) {
 	}
 
 	return &InternalDocument{
-		key:           d.key,
-		status:        d.status,
-		checkpoint:    d.checkpoint,
-		changeID:      d.changeID,
+		key:        d.key,
+		status:     d.status,
+		checkpoint: d.checkpoint,
+
+		// TODO(hackerwins): Previously ChangeID used as an immutable value,
+		// but now it is mutable, so we need to create a new instance of ChangeID.
+		// COnsider removing this in the future.
+		changeID: d.changeID.DeepCopy(),
+
 		root:          root,
 		presences:     d.presences.DeepCopy(),
 		onlineClients: &gosync.Map{},

server/packs/packs.go
@@ -250,14 +250,21 @@ func BuildInternalDocForServerSeq(
 	serverSeq int64,
 ) (*document.InternalDocument, error) {
 	docKey := docInfo.RefKey()
+	var doc *document.InternalDocument
+	var err error
+	if cached, ok := be.SnapshotCache.Get(docKey); ok {
+		doc, err = cached.DeepCopy()
+		if err != nil {
+			return nil, err
+		}
+	}
 
 	// NOTE(hackerwins): If the document is already in the cache, we can skip
 	// the database query and use the cached document. If the document's server
 	// sequence in the cache is greater than the given server sequence, we can't
 	// build the document from the document. In this case, we need to
 	// query the database to get the closest snapshot information.
-	doc, ok := be.SnapshotCache.Get(docKey)
-	if !ok || serverSeq < doc.Checkpoint().ServerSeq {
+	if doc == nil || serverSeq < doc.Checkpoint().ServerSeq {
 		snapshotInfo, err := be.DB.FindClosestSnapshotInfo(
 			ctx,
 			docKey,
@@ -299,7 +306,6 @@ func BuildInternalDocForServerSeq(
 	), be.Config.SnapshotDisableGC); err != nil {
 		return nil, err
 	}
-
 	if !be.Config.SnapshotDisableGC {
 		vector, err := be.DB.GetMinVersionVector(
 			ctx,
@@ -314,12 +320,8 @@ func BuildInternalDocForServerSeq(
 		}
 	}
 
-	// NOTE(hackerwins): Store the latest document in the cache.
-	clone, err := doc.DeepCopy()
-	if err != nil {
-		return nil, err
-	}
-	be.SnapshotCache.Add(docKey, clone)
+	// NOTE(hackerwins): Store the last accessed document in the cache.
+	be.SnapshotCache.Add(docKey, doc)
 
 	if logging.Enabled(zap.DebugLevel) {
 		logging.From(ctx).Debugf(
@@ -331,7 +333,12 @@ func BuildInternalDocForServerSeq(
 		)
 	}
 
-	return doc, nil
+	clone, err := doc.DeepCopy()
+	if err != nil {
+		return nil, err
+	}
+
+	return clone, nil
 }
 
 // Compact compacts the given document and its metadata and stores them in the

test/integration/admin_test.go
@@ -95,7 +95,6 @@ func TestAdmin(t *testing.T) {
 		assert.NoError(t, err)
 		assert.NoError(t, cli.Activate(ctx))
 		defer func() {
-			assert.NoError(t, cli.Deactivate(ctx))
 			assert.NoError(t, cli.Close())
 		}()
 
@@ -113,6 +112,7 @@ func TestAdmin(t *testing.T) {
 			return nil
 		}))
 		assert.Equal(t, int32(1), doc.Root().GetCounter("counter").Value())
+		assert.NoError(t, cli.Sync(ctx))
 
 		_, err = adminCli.UpdateDocument(ctx, "default", doc.Key(), yson.Object{
 			"counter": yson.Counter{Type: crdt.IntegerCnt, Value: 0},

test/integration/restapi_test.go
@@ -19,16 +19,23 @@
 package integration
 
 import (
-	"encoding/json"
+	"context"
+	gojson "encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"strings"
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/yorkie-team/yorkie/api/types"
+	"github.com/yorkie-team/yorkie/client"
+	"github.com/yorkie-team/yorkie/pkg/document"
+	"github.com/yorkie-team/yorkie/pkg/document/json"
+	"github.com/yorkie-team/yorkie/pkg/document/presence"
+	"github.com/yorkie-team/yorkie/pkg/document/yson"
 	"github.com/yorkie-team/yorkie/test/helper"
 )
 
@@ -53,7 +60,7 @@ func TestRESTAPI(t *testing.T) {
 		)
 
 		summary := &documentSummary{}
-		assert.NoError(t, json.Unmarshal(res, summary))
+		assert.NoError(t, gojson.Unmarshal(res, summary))
 		assert.Equal(t, docs[0].Key(), summary.Document.Key)
 	})
 
@@ -67,7 +74,7 @@ func TestRESTAPI(t *testing.T) {
 		)
 
 		summaries := &documentSummaries{}
-		assert.NoError(t, json.Unmarshal(res, summaries))
+		assert.NoError(t, gojson.Unmarshal(res, summaries))
 		assert.Len(t, summaries.Documents, 2)
 	})
 
@@ -81,7 +88,7 @@ func TestRESTAPI(t *testing.T) {
 		)
 
 		summaries := &documentSummaries{}
-		assert.NoError(t, json.Unmarshal(res, summaries))
+		assert.NoError(t, gojson.Unmarshal(res, summaries))
 		assert.Len(t, summaries.Documents, 3)
 	})
 
@@ -94,7 +101,7 @@ func TestRESTAPI(t *testing.T) {
 			fmt.Sprintf(`{"project_name": "%s", "query": "0-", "page_size": 3}`, project.Name),
 		)
 		summaries := &documentSummaries{}
-		assert.NoError(t, json.Unmarshal(res, summaries))
+		assert.NoError(t, gojson.Unmarshal(res, summaries))
 		assert.Len(t, summaries.Documents, 1)
 
 		_ = post(
@@ -111,8 +118,70 @@ func TestRESTAPI(t *testing.T) {
 			fmt.Sprintf(`{"project_name": "%s", "query": "0-", "page_size": 3}`, project.Name),
 		)
 		summaries = &documentSummaries{}
-		assert.NoError(t, json.Unmarshal(res, summaries))
+		assert.NoError(t, gojson.Unmarshal(res, summaries))
 		assert.Len(t, summaries.Documents, 0)
+	})
+
+	t.Run("concurrent document retrieval test", func(t *testing.T) {
+		project, docs := helper.CreateProjectAndDocuments(t, defaultServer, 1)
+
+		res := post(
+			t,
+			project,
+			fmt.Sprintf("http://%s/yorkie.v1.AdminService/GetDocument", defaultServer.RPCAddr()),
+			fmt.Sprintf(`{"project_name": "%s", "document_key": "%s"}`, project.Name, docs[0].Key()),
+		)
+		summary := &documentSummary{}
+		assert.NoError(t, gojson.Unmarshal(res, summary))
+		assert.Equal(t, "{}", summary.Document.Snapshot)
+
+		ctx := context.Background()
+
+		cli, err := client.Dial(defaultServer.RPCAddr(), client.WithAPIKey(project.PublicKey))
+		assert.NoError(t, err)
+		assert.NoError(t, cli.Activate(ctx))
+		defer cli.Close()
+
+		doc := document.New(docs[0].Key())
+		assert.NoError(t, cli.Attach(ctx, doc))
+		assert.NoError(t, doc.Update(func(r *json.Object, p *presence.Presence) error {
+			r.SetYSON(yson.Object{"arr": yson.Array{}})
+			return nil
+		}))
+		cli.Sync(ctx)
+
+		res = post(
+			t,
+			project,
+			fmt.Sprintf("http://%s/yorkie.v1.AdminService/GetDocument", defaultServer.RPCAddr()),
+			fmt.Sprintf(`{"project_name": "%s", "document_key": "%s"}`, project.Name, docs[0].Key()),
+		)
+		assert.NoError(t, gojson.Unmarshal(res, summary))
+		assert.Equal(t, `{"arr":[]}`, summary.Document.Snapshot)
+
+		assert.NoError(t, doc.Update(func(r *json.Object, p *presence.Presence) error {
+			r.GetArray("arr").AddInteger(1)
+			return nil
+		}))
+		cli.Sync(ctx)
+
+		wg := sync.WaitGroup{}
+		for range 10 {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				res := post(
+					t,
+					project,
+					fmt.Sprintf("http://%s/yorkie.v1.AdminService/GetDocument", defaultServer.RPCAddr()),
+					fmt.Sprintf(`{"project_name": "%s", "document_key": "%s"}`, project.Name, docs[0].Key()),
+				)
+				summary := &documentSummary{}
+				assert.NoError(t, gojson.Unmarshal(res, summary))
+				assert.Equal(t, `{"arr":[1]}`, summary.Document.Snapshot)
+			}()
+		}
+		wg.Wait()
 	})
 }
 