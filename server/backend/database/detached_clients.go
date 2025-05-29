5d4d3c4b089d3cde7141de0d55b05c26988a3f7c
JOOHOJANG <jayjangjoo@gmail.com>
2025-05-30 01:51:09 +0900

    Define detached clients column in db

---
+++ server/backend/database/detached_clients.go
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2025 The Yorkie Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package database
+
+import "github.com/yorkie-team/yorkie/api/types"
+
+// DetachedClients is a structure representing information about the detached client's lamport for each document and client.
+type DetachedClients struct {
+	ID        types.ID `bson:"_id"`
+	ProjectID types.ID `bson:"project_id"`
+	DocID     types.ID `bson:"doc_id"`
+	ClientID  types.ID `bson:"client_id"`
+	Lamport   int64    `bson:"lamport"`
+	ServerSeq int64    `bson:"server_seq"`
+}
