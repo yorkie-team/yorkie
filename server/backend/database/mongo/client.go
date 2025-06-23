88254159035e1e4f49a4489a1cc9209d4b2094fb
emplam27 <emplam27@gmail.com>
2025-06-23 15:38:50 +0900

    Refactor CreateChangeInfos method to use refKey for document updates and adjust error message formatting

--- server/backend/database/mongo/client.go
+++ server/backend/database/mongo/client.go
@@ -1074,7 +1074,7 @@ func (c *Client) GetClientsCount(ctx context.Context, projectID types.ID) (int64
 // CreateChangeInfos stores the given changes and doc info.
 func (c *Client) CreateChangeInfos(
 	ctx context.Context,
-	docRefKey types.DocRefKey,
+	refKey types.DocRefKey,
 	checkpoint change.Checkpoint,
 	changes []*database.ChangeInfo,
 	isRemoved bool,
@@ -1146,9 +1146,9 @@ func (c *Client) CreateChangeInfos(
 	}
 
 	res, err := c.collection(ColDocuments).UpdateOne(ctx, bson.M{
-		"project_id": docRefKey.ProjectID,
-		"_id":        docRefKey.DocID,
-		"key":        docRefKey.DocKey,
+		"project_id": refKey.ProjectID,
+		"_id":        refKey.DocID,
+		"key":        refKey.DocKey,
 		"server_seq": initialServerSeq,
 	}, bson.M{
 		"$set": updateFields,
@@ -1159,7 +1159,7 @@ func (c *Client) CreateChangeInfos(
 	}
 	if res.MatchedCount == 0 {
 		c.docCache.Remove(refKey)
-		return nil, change.InitialCheckpoint, fmt.Errorf("update document: %s, %s: %w", refKey, docInfoKey, database.ErrConflictOnUpdate)
+		return nil, change.InitialCheckpoint, fmt.Errorf("update document: %s: %w", refKey, database.ErrConflictOnUpdate)
 	}
 
 	if isRemoved {
--- server/packs/pushpull.go
+++ server/packs/pushpull.go
@@ -159,7 +159,7 @@ func pushPack(
 	clientInfo *database.ClientInfo,
 	docKey types.DocRefKey,
 	reqPack *change.Pack,
-) ([]*change.Change, *database.DocInfo, int64, change.Checkpoint, error) {
+) ([]*database.ChangeInfo, *database.DocInfo, int64, change.Checkpoint, error) {
 	cpBeforePush := clientInfo.Checkpoint(docKey.DocID)
 
 	// 01. Filter out changes that are already pushed.
