8e3d6064330a6673700426ca23d0075d397ae082
emplam27 <emplam27@gmail.com>
2025-06-25 21:16:50 +0900

    Update initial chunk splitting logic from init-mongos1.js

--- build/docker/sharding/scripts/init-mongos1.js
+++ build/docker/sharding/scripts/init-mongos1.js
@@ -1,20 +1,6 @@
 sh.addShard("shard-rs-1/shard1-1:27017");
 sh.addShard("shard-rs-2/shard2-1:27017");
 
-function findAnotherShard(shard) {
-  if (shard == "shard-rs-1") {
-    return "shard-rs-2";
-  } else {
-    return "shard-rs-1";
-  }
-}
-
-function shardOfChunk(minKeyOfChunk) {
-  return db
-    .getSiblingDB("config")
-    .chunks.findOne({ min: { project_id: minKeyOfChunk } }).shard;
-}
-
 // Shard the database for the mongo client test
 const mongoClientDB = "test-yorkie-meta-mongo-client";
 sh.enableSharding(mongoClientDB);
@@ -23,16 +9,6 @@ sh.shardCollection(mongoClientDB + ".documents", { key: "hashed" });
 sh.shardCollection(mongoClientDB + ".changes", { doc_id: "hashed" });
 sh.shardCollection(mongoClientDB + ".snapshots", { doc_id: "hashed" });
 sh.shardCollection(mongoClientDB + ".versionvectors", { doc_id: "hashed" });
-
-// Split the inital range at `splitPoint` to allow doc_ids duplicate in different shards.
-const splitPoint = ObjectId("500000000000000000000000");
-sh.splitAt(mongoClientDB + ".documents", { project_id: splitPoint });
-// Move the chunk to another shard.
-db.adminCommand({
-  moveChunk: mongoClientDB + ".documents",
-  find: { project_id: splitPoint },
-  to: findAnotherShard(shardOfChunk(splitPoint)),
-});
 
 // Shard the database for the server test
 const serverDB = "test-yorkie-meta-server";
